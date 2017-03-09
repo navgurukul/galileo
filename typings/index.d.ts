ec96f3ed48d949d084a8a33b4a25c6fc8707e9b5
Rishabh Verma <me@rishabhverma.me>
2017-03-10 00:30:14 +0530

    The dreaded hapi typings included.

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -7,3 +7,4 @@
 /// <reference path="globals/mocha/index.d.ts" />
 /// <reference path="globals/nconf/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
+/// <reference path="modules/hapi/index.d.ts" />
