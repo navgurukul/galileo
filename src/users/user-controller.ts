acade7d62da9f1787ee6c186dc46c82562e1b0f6
vidur <vidursingla13@gmail.com>
2017-04-12 17:20:45 +0530

    added login-logic

--- src/users/user-controller.ts
+++ src/users/user-controller.ts
@@ -19,7 +19,39 @@ export default class UserController {
     }
 
     public loginUser(request: Hapi.Request, reply: Hapi.IReply) {
-        return reply({ "jwt": "xxx.yyy.zzz" });
+        let requestPayload:any = request.payload;
+        let auth = new GoogleAuth;
+        let client = new auth.OAuth2(this.configs.googleAuth.clientId, '', '');
+        client.verifyIdToken(requestPayload.idToken, this.configs.googleAuth.clientId, (e, login) => {
+          let googleAuthPayload = login.getPayload();
+          // Check if user has navgurukul.org eMail-ID
+          if (googleAuthPayload['hd'] !== 'navgurukul.org') {
+              return reply(Boom.unauthorized("You need to have a navgurukul.org email to access this."));
+          }
+        //   Check if a user with an email already exists
+          this.database('users').select().where('email', googleAuthPayload['email']).then(
+            (rows) => {
+              if (rows.length === 0) {
+                this.database('users').insert({email: googleAuthPayload['email'],
+                name: googleAuthPayload['name'], profilePicture: googleAuthPayload['picture']}).then(
+                  (response) => {
+                    return Promise.resolve({
+                      id:response[0],
+                      email: googleAuthPayload['email']
+                    });
+                  }
+                );
+              } else {
+                let user = rows[0];
+                return Promise.resolve({id: user['id'], email: user['email']});
+              }
+          }
+        ).then((res) => {
+          let token = Jwt.sign({email: res.email, id: res.id}, "secret", {expiresIn: "24h"});
+          console.log(token);
+          return reply({"jwt": token});
+        });
+      });
     }
 
     public getUserInfo(request: Hapi.Request, reply: Hapi.IReply) {
