eb2963f6b50798e0f24df6d91cab7aa8cd4dcc93
Amar Kumar Sinha <amar17@navgurukul.org>
2018-10-23 23:23:09 +0530

    change env to prod

--- courseUpdate.sh
+++ courseUpdate.sh
@@ -5,7 +5,7 @@ echo "(Unless Error Message is attached here.)"
 
 #cd /home/galileo
 
-export GALILEO_ENV='dev'
+export GALILEO_ENV='prod'
 cd curriculum
 git pull > /dev/null
 cd ..
@@ -13,7 +13,6 @@ if [ "$1" == "all" ]
 then
 	for input in $(find . -name "info.md" | awk -F'\/' '{print $3}')
 	do
-		# node --inspect ./node_modules/.bin/ts-node --transpile-only src/seed-courses/index.ts --courseDir $input
 		ts-node src/seed-courses/index.ts --courseDir $input
 	done
 else
