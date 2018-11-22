3bb8a8ab45e2f6804694858fc409969c4207b1a6
Amar Kr Sinha <amar17@navgurukul.org>
2018-11-22 19:40:43 +0530

    promises error resolved (#71)
    * created a route to delete courses from backend #66
    
    * changes
    
    * added isAdmin parameter for admin
    
    * added missing line database.ts
    
    * resolved promises problem

--- src/controllers/users/user-controller.ts
+++ src/controllers/users/user-controller.ts
@@ -44,7 +44,8 @@ export default class UserController {
                         .where({'id': user.id})
                         .then((rows) => {
                             if(rows.length < 1){
-                                database('user_roles').insert({
+                                return database('user_roles').insert({
+                                  database('user_roles').insert({
                                     userId: user.id,
                                   })
                                   .then((row) => {
