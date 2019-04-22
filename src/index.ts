b36fb8e6073a3347248c3a6482b83f80d7240236
Santra <koushik.santra@accenture.com>
2019-04-22 12:28:24 +0530

    Update on the schedule issue #112

--- src/index.ts
+++ src/index.ts
@@ -98,16 +98,16 @@ const server = Server.init(serverConfigs, databaseConfig)
                 second ( optional )
             **/
 
-            // cron.schedule(`${scheduleConfigs.timeInSecond} ${scheduleConfigs.minute} ${scheduleConfigs.hour} ${scheduleConfigs.dayOfMonth} ${scheduleConfigs.month} ${scheduleConfigs.dayOfWeek}`, function () {
-            //     console.log("Running Cron Job");
+            cron.schedule(`${scheduleConfigs.timeInSecond} ${scheduleConfigs.minute} ${scheduleConfigs.hour} ${scheduleConfigs.dayOfMonth} ${scheduleConfigs.month} ${scheduleConfigs.dayOfWeek}`, function () {
+                console.log("Running Cron Job");
                 
-            //     request(server.info.uri+'/reports/getSubmissionReport', function (error, response, body) {
-            //         if (!error && response.statusCode == 200) {
+                request(server.info.uri+'/reports/getSubmissionReport', function (error, response, body) {
+                    if (!error && response.statusCode == 200) {
                         
-            //              //
-            //         }
-            //     })
-            // });
+                         //
+                    }
+                })
+            });
 
 
 
