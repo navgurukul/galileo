c10e584c649f9b9a4385d758fb775f9ca138a9b9
amarkumarsinha <amarkr.sinha@thoughtworks.com>
2020-01-14 19:37:30 +0530

    [Amar][WIP] Add. Business Error Plugins.

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -1,14 +1,15 @@
-module.exports =  {
-  loadBefore: [
-    require('./schmervice/index'),
-    require('./schwifty/index'),
-    require('./swagger/index'),
-  ],
-  onlyForProduction: [
-    require('./logger/index'),
-    require('./sentry-behaviour-tracker/index')
-  ],
-  loadAfter: [
-    require('./jwt-auth/index'), // Need to access services which can be after schmservice is registered.
-  ],
+module.exports = {
+	loadBefore: [
+		require('./schmervice/index'),
+		require('./schwifty/index'),
+		require('./swagger/index'),
+		require('./business-errors')
+	],
+	onlyForProduction: [
+		require('./logger/index'),
+		require('./sentry-behaviour-tracker/index')
+	],
+	loadAfter: [
+		require('./jwt-auth/index'), // Need to access services which can be after schmservice is registered.
+	],
 };
