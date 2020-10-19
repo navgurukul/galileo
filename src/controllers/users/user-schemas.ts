e7a04478daf495be9dbeccf4b73e1dbb20fffc8e
pysaquib <iamsaquibnasim@gmail.com>
2020-10-19 22:21:26 +0530

    added chat id and password on user schema

--- src/controllers/users/user-schemas.ts
+++ src/controllers/users/user-schemas.ts
@@ -13,7 +13,9 @@ export const userSchema: Joi.ObjectSchema = Joi.object({
     created_at: Joi.date().allow(null),
     github_link: Joi.string().regex(/(ftp|http|https):\/\/?(?:www\.)?github.com(\w+:{0,1}\w*@)?(\S+)(:([0-9])+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/).allow(null).uri(),
     linkedin_link: Joi.string().regex(/(ftp|http|https):\/\/?(?:www\.)?linkedin.com(\w+:{0,1}\w*@)?(\S+)(:([0-9])+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/).allow(null).uri(),
-    medium_link: Joi.string().regex(/(ftp|http|https):\/\/?(?:www\.)?medium.com(\w+:{0,1}\w*@)?(\S+)(:([0-9])+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/).allow(null).uri()
+    medium_link: Joi.string().regex(/(ftp|http|https):\/\/?(?:www\.)?medium.com(\w+:{0,1}\w*@)?(\S+)(:([0-9])+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/).allow(null).uri(),
+    chat_id: Joi.string(),
+    chat_password: Joi.string()
 });
 
 
